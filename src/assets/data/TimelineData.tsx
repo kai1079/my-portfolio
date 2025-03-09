89e12f4163caa884adf352ff982e6d6b3e81c195
kaijr <pckhang10@gmail.com>
2025-03-09 23:08:35 +0700

    Refactor code

--- src/assets/data/TimelineData.tsx
+++ src/assets/data/TimelineData.tsx
@@ -4,45 +4,45 @@ import { BuildingOffice2Icon, CodeBracketSquareIcon, RocketLaunchIcon } from "@h
 import { TimelineItemData } from "../../types/TimelineType";
 
 export const timelineData: TimelineItemData[] = [
-    {
-      company: "THOUGHTWORKS",
-      title: "SOFTWARE ENGINEERING CONSULTANT",
-      project: [
-        {
-          title: "Project: Global Mosquito-Borne Disease Protection Initiative",
-          description: "Improved and designed CI/CD pipelines using GoCD to ensure efficient build and deployment processes integrated with AWS EC2. Enhanced automation and reliability of the deployment pipeline to support global health initiatives.",
-        },
-        {
-          title: "Project: Accounting Software for Australian SME Clients",
-          description: "Maintained, developed, and improved the database management service (REST API) for accounting software supporting small and medium-sized businesses. Developed the proxy gateway service that integrated with cloud finance reporting third party Silverfin to generate financial reporting. Collaborated in the design, development, and implementation of new features, improving software functionality and user experience. Provided technical solutions and consulting to optimize system performance and address client needs"
-        }
-      ],
-      date: "NOV 2022 - PRESENT",
-      logo: "/images/assets/tw.png",
-      icon: BuildingOffice2Icon,
-      skills: ['AWS', 'Docker', 'Kubernetes', 'Python', 'Terraform']
-    },
-    {
-      company: "TIKI CORPORATION ",
-      title: "BACKEND DEVELOPER - CORE PLATFORM",
-      project: [
-        {
-          title: "Project: Core Architect Platform",
-          description: "Contributed to the development and enhancement of Tiki E-commerce platform by coding and optimizing backend services. Developed and maintained REST APIs using Java and Spring Boot, ensuring efficient, secure communication between services. Implemented Multi-Factor Authentication (MFA) solutions, including SMS, OTP, and Email, to enhance platform security. Leveraged technologies such as Kafka for messaging, Redis for caching, and RabbitMQ for event-driven architecture to improve system scalability and performance. Worked with MySQL to design and optimize databases, ensuring high performance and reliability. Developed secure APIs with JWT and OAuth for authentication and authorization. Conducted thorough API testing using Postman to ensure functionality and performance"
-        }
-      ],
-      date: "JAN 2022 - NOV 2022",
-      logo: "/images/assets/tiki.png",
-      icon: CodeBracketSquareIcon,
-      skills: ['AWS', 'Docker', 'Kubernetes', 'Python', 'Terraform']
-    },
-    {
-      company: "DEK - ENDAVA",
-      title: "SOFTWARE ENGINEERING CONSULTANT",
-      project: [
-        {
-          title: "Project: CBA (Component-Based Architecture)",
-          description: "Contributed to the development of core middleware components bridging the application layer and the operating system layer in a distributed, cluster-based environment. \
+  {
+    company: "THOUGHTWORKS",
+    title: "SOFTWARE ENGINEERING CONSULTANT",
+    project: [
+      {
+        title: "Project: Global Mosquito-Borne Disease Protection Initiative",
+        description: "Improved and designed CI/CD pipelines using GoCD to ensure efficient build and deployment processes integrated with AWS EC2. Enhanced automation and reliability of the deployment pipeline to support global health initiatives.",
+      },
+      {
+        title: "Project: Accounting Software for Australian SME Clients",
+        description: "Maintained, developed, and improved the database management service (REST API) for accounting software supporting small and medium-sized businesses. Developed the proxy gateway service that integrated with cloud finance reporting third party Silverfin to generate financial reporting. Collaborated in the design, development, and implementation of new features, improving software functionality and user experience. Provided technical solutions and consulting to optimize system performance and address client needs"
+      }
+    ],
+    date: "NOV 2022 - PRESENT",
+    logo: "/images/assets/tw.png",
+    icon: BuildingOffice2Icon,
+    skills: ["C#", ".NET", ".NET Core", "React", "TypeScript", "AWS", "Buildkite", "Kubernetes", "Docker", "CloudFront"]
+  },
+  {
+    company: "TIKI CORPORATION ",
+    title: "BACKEND DEVELOPER - CORE PLATFORM",
+    project: [
+      {
+        title: "Project: Core Architect Platform",
+        description: "Contributed to the development and enhancement of Tiki E-commerce platform by coding and optimizing backend services. Developed and maintained REST APIs using Java and Spring Boot, ensuring efficient, secure communication between services. Implemented Multi-Factor Authentication (MFA) solutions, including SMS, OTP, and Email, to enhance platform security. Leveraged technologies such as Kafka for messaging, Redis for caching, and RabbitMQ for event-driven architecture to improve system scalability and performance. Worked with MySQL to design and optimize databases, ensuring high performance and reliability. Developed secure APIs with JWT and OAuth for authentication and authorization. Conducted thorough API testing using Postman to ensure functionality and performance"
+      }
+    ],
+    date: "JAN 2022 - NOV 2022",
+    logo: "/images/assets/tiki.png",
+    icon: CodeBracketSquareIcon,
+    skills: ["Java", "Spring Boot", "Kafka", "Redis", "RabbitMQ", "MySQL", "JWT", "OAuth"]
+  },
+  {
+    company: "DEK - ENDAVA",
+    title: "SOFTWARE ENGINEERING CONSULTANT",
+    project: [
+      {
+        title: "Project: CBA (Component-Based Architecture)",
+        description: "Contributed to the development of core middleware components bridging the application layer and the operating system layer in a distributed, cluster-based environment. \
   Participated in the full software development lifecycle, including feature study, design, \
   implementation, testing, and documentation. \
   Worked with OpenSAF, an open-source platform for automating development, scaling, and \
@@ -52,11 +52,13 @@ export const timelineData: TimelineItemData[] = [
   Packaged source code using Autotools and Makefile for efficient build processes. \
   Collaborated in Agile environments with tools like Scrum, Jira, Jenkins, Confluence, Git, and \
   Gerrit for code review and version control.",
-        }
-      ],
-      date: "JAN 2019 - JAN 2022",
-      logo: "/images/assets/dek.jpg",
-      icon: RocketLaunchIcon,
-      skills: ['AWS', 'Docker', 'Kubernetes', 'Python', 'Terraform']
-    }
-  ];
+      }
+    ],
+    date: "JAN 2019 - JAN 2022",
+    logo: "/images/assets/dek.jpg",
+    icon: RocketLaunchIcon,
+    skills: ["C/C++", "Python", "Java", "Bash Script",
+      "OpenSAF", "Autotools", "Makefile", "TestNG",
+      "Jira", "Jenkins", "Git", "Gerrit"]
+  }
+];
